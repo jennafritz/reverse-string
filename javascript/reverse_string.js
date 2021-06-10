893822b931217f5b879bd28a2f1430fe2e045ef5
jennafritz <jennafritz24@gmail.com>
2021-06-10 12:16:12 -0400

    commit tree 17f15f786f12cdf4d547ba5619c5a86c7d80b882

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
   // type your code here
+  let result = []
+  split = str.split("")
+  for (i = split.length; i >= 0; i--) {
+    result.push(split[i])
+  }
+  return result.join("")
 }
 
 if (require.main === module) {
@@ -11,9 +17,19 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  // my tests
+  console.log("Expecting: 'tset'")
+  console.log("=>", reverseString("test"))
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+  // initialize empty array called results
+  // split string into array
+  // iterate through array starting at last element
+  // push each element into results array
+  // return results array joined at commas
+
 // And a written explanation of your solution
--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,17 +1,25 @@
 def reverse_string(str)
   # type your code in here
+  result = []
+  split = str.split("")
+  counter = split.count
+  while counter >= 0
+    result << split[counter]
+    counter -= 1
+  end
+  result.join("")
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
   puts "=>", reverse_string('hi')
 
-  puts
-
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
 
   # Don't forget to add your own!
+  puts "Expecting: 'tset'"
+  puts "=>", reverse_string('test')
 end
 
 # Please add your pseudocode to this file
